0adb07928d36f6d9b15d4b67841bce7a891ca4d3
timfi <timfi@ADELL>
2025-12-14 13:25:33 +0200

    Add project files.

---
+++ DemoFrontEnd/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using DemoFrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoFrontEnd.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public JsonResult ValidateLogin(SystemUser model)
+        {
+            var result = new Dictionary<string, bool>();
+            result.Add("username", model.Username == "admin" ? true : false);
+            result.Add("password", model.Password == "1234" ? true : false);
+            return Json(result);
+        }
+    }
+}
---
+++ DemoFrontEnd/Models/SystemUser.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoFrontEnd.Models
+{
+    public class SystemUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
